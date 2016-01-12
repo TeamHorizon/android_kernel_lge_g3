9e095cd10235a98e6c8f35e3b1370de7125463d8
Loller79 <loller790@gmail.com>
2016-01-12 17:37:14 +0100

    Raise Thresholds to Fix 4 Cores Always Online
    Change-Id: I190867cbd823326b4b17b5ad0ee13f10b1e35536

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -29,8 +29,8 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(16)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(15)
+#define DEF_FREQUENCY_UP_THRESHOLD		(55)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(54)
 
 /*
  * The polling frequency of this governor depends on the capability of
